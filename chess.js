656b191948ae6753935d4fe336f64f9339eca242
Callmeisml <129768621+Callmeisml@users.noreply.github.com>
2023-05-11 22:21:25 +0200

    Add files via upload

---
+++ chess.js
@@ -0,0 +1,29 @@
+// html
+
+
+
+// board
+
+let board = [];
+
+let casesNames;
+let file = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let name;
+let n = 0;
+let Case;
+
+file.forEach(element => {
+    for (let i = 0; i < 8; i++) {
+        n++;
+        x = element + (i+1);
+        Case = {
+            row: i,
+            file: 
+            id: n,
+            name: x,
+            piece: ''
+        };
+        board.push(Case);
+    }
+    
+});
